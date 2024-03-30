c47dba168e3b5e361846eda6cd552757e38cfa61
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2024-03-29 22:15:45 -0700

    basic styling for the Home Page

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,4 +1,3 @@
-// Home.js
 import React from "react";
 import { Link } from "react-router-dom";
 import { useN } from "../contexts/Context";
@@ -6,20 +5,49 @@ import { useN } from "../contexts/Context";
 const Home = () => {
   const { n, setN } = useN();
 
+  const inputStyles = {
+    marginTop: "5px",
+    marginBottom: "10px",
+    padding: "8px",
+    fontSize: "16px",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    width: "400px",
+  };
+
+  const buttonStyles = {
+    padding: "10px 20px",
+    fontSize: "16px",
+    backgroundColor: "#007bff",
+    color: "#fff",
+    border: "none",
+    borderRadius: "4px",
+    cursor: "pointer",
+    textDecoration: "none",
+  };
+
+  const formStyles = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  };
+
   return (
     <div>
-      <form>
+      <form style={formStyles}>
         <label>
-          Enter n:
+          <br />
           <input
             type="number"
             value={n}
             onChange={(e) => setN(e.target.value)}
             required
+            style={inputStyles}
+            placeholder="Enter the count"
           />
         </label>
-        <Link to="/fib-output">
-          <button>Submit</button>
+        <Link to="/fib-output" style={{ textDecoration: "none" }}>
+          <button style={buttonStyles}>Submit</button>
         </Link>
       </form>
     </div>
