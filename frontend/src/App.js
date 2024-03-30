c5a53e2f3705cac29b9e31e69d96cdc1be79ba3f
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2024-03-29 20:56:03 -0700

    added react router and views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,18 @@
-import React, { useState } from "react";
-import { calculateFib } from "./controllers/fib";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// All pages go here
+import Home from "./views/Home";
 
 function App() {
-  const [n, setN] = useState("");
-  const [fibNumbers, setFibNumbers] = useState([]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await calculateFib({ n });
-      setFibNumbers(response.data.fibNumbers);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter n:
-          <input
-            type="number"
-            value={n}
-            onChange={(e) => setN(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      <ul>
-        {fibNumbers.map((number, index) => (
-          <li key={index}>{number}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
